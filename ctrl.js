6e600d4fd7687a94edd6c70d8be1d876408a05f9
jeannie.boffel <jboffel@gmail.com>
2016-02-10 09:29:47 +0900

    Add daemonization

---
+++ ctrl.js
@@ -0,0 +1,23 @@
+/**
+ * Created by jeannie.boffel on 2/10/2016.
+ */
+
+var daemon = require("daemonize2").setup({
+    main: "../bin/www",
+    name: "agilepoker",
+    pidfile: "/var/run/agilepoker.pid"
+});
+
+switch (process.argv[2]) {
+
+    case "start":
+        daemon.start();
+        break;
+
+    case "stop":
+        daemon.stop();
+        break;
+
+    default:
+        console.log("Usage: [start|stop]");
+}
